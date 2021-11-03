3c1c72cc23d436527464b97509ff1d361cbd723d
Astro Orbis <astroorbis@gmail.com>
2021-11-03 15:09:44 -0400

    Added SHA256 and SHA512 hashing.

--- encrypt.go
+++ encrypt.go
@@ -3,6 +3,8 @@ package main
 import (
 	"crypto/md5"
 	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/hex"
 )
 
@@ -32,4 +34,32 @@ func verboseSHA1encrypt(text string) string {
 	hasher.Write([]byte(text))
 	hash := hex.EncodeToString(hasher.Sum(nil))
 	return "SHA1 hash for " + text + ": " + hash
+}
+
+func SHA256encrypt(text string) string {
+	hasher := sha256.New()
+	hasher.Write([]byte(text))
+	hash := hex.EncodeToString(hasher.Sum(nil))
+	return hash
+}
+
+func verboseSHA256encrypt(text string) string {
+	hasher := sha256.New()
+	hasher.Write([]byte(text))
+	hash := hex.EncodeToString(hasher.Sum(nil))
+	return "SHA256 hash for " + text + ": " + hash
+}
+
+func SHA512encrypt(text string) string {
+	hasher := sha512.New()
+	hasher.Write([]byte(text))
+	hash := hex.EncodeToString(hasher.Sum(nil))
+	return hash
+}
+
+func verboseSHA512encrypt(text string) string {
+	hasher := sha512.New()
+	hasher.Write([]byte(text))
+	hash := hex.EncodeToString(hasher.Sum(nil))
+	return "SHA512 hash for " + text + ": " + hash
 }
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
 )
 
 // https://gobyexample.com/command-line-flags
@@ -12,48 +11,74 @@ import (
 func main() {
 	md5flag := flag.Bool("md5", false, "Returns a md5 hash.")
 	sha1flag := flag.Bool("sha1", false, "Returns a sha1 hash.")
-
+	sha256flag := flag.Bool("sha256", false, "Returns a sha256 hash.")
+	sha512flag := flag.Bool("sha512", false, "Returns a sha512 hash.")
 	verboseflag := flag.Bool("v", false, "Returns verbose output.")
+
+	stringflag := flag.String("string", "", "Hashes the given string.")
+	// StrArg := strings.Join(flag.Args(), " ")
 
 	flag.Parse()
 
-	StrArg := strings.Join(flag.Args(), " ")
-
-	// Chcek for multiple args
-	if strings.Contains(StrArg, " ") {
-		fmt.Println("Please only specify 1 string to hash.")
+	// Check for multiple hashtypes and lack of flags
+	hashtypeflaglist := 0
+	if *md5flag {
+		hashtypeflaglist++
+	}
+	if *sha1flag {
+		hashtypeflaglist++
+	}
+	if *sha256flag {
+		hashtypeflaglist++
+	}
+	if *sha512flag {
+		hashtypeflaglist++
 	}
 
-	// Check for multiple hashtypes
-	if *md5flag && *sha1flag {
+	if hashtypeflaglist > 1 {
 		fmt.Println("Please use only 1 hashtype flag.")
 		os.Exit(1) //https://shapeshed.com/unix-exit-codes/
 	}
 
-	// Check for lack of flags
-	if !*md5flag && !*sha1flag {
+	if hashtypeflaglist == 0 {
 		fmt.Println("Please provide one hashtype flag and one (or multiple) argument(s). All flags can be listed with the -h flag.")
 	}
 
-	// Checks for verbosity
+	// TODO: Update the switcher
 	if *verboseflag {
 
 		if *md5flag {
-			fmt.Println(verboseMD5encrypt(StrArg))
+			fmt.Println(verboseMD5encrypt(*stringflag))
 		}
 
 		if *sha1flag {
-			fmt.Println(verboseSHA1encrypt(StrArg))
+			fmt.Println(verboseSHA1encrypt(*stringflag))
+		}
+
+		if *sha256flag {
+			fmt.Println(verboseSHA256encrypt(*stringflag))
+		}
+
+		if *sha512flag {
+			fmt.Println(verboseSHA512encrypt(*stringflag))
 		}
 
 	} else {
 
 		if *md5flag {
-			fmt.Println(MD5encrypt(StrArg))
+			fmt.Println(MD5encrypt(*stringflag))
 		}
 
 		if *sha1flag {
-			fmt.Println(SHA1encrypt(StrArg))
+			fmt.Println(SHA1encrypt(*stringflag))
+		}
+
+		if *sha256flag {
+			fmt.Println(SHA256encrypt(*stringflag))
+		}
+
+		if *sha512flag {
+			fmt.Println(SHA512encrypt(*stringflag))
 		}
 
 	}
